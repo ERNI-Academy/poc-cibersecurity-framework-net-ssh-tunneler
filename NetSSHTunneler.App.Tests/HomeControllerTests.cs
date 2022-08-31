f178e15edbb99f0691db8c8a740363ed0d98f90a
Samuel Hernández <s.hernandezmartos@gmail.com>
2022-08-31 17:26:45 +0200

    Add AutoMoq to the project

--- NetSSHTunneler.App.Tests/HomeControllerTests.cs
+++ NetSSHTunneler.App.Tests/HomeControllerTests.cs
@@ -1,60 +1,40 @@
+using AutoFixture.Xunit2;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Moq;
 using NetSSHTunneler.Controllers;
 using NetSSHTunneler.Domain.DTOs;
 using NetSSHTunneler.Domain.Responses;
 using NetSSHTunneler.Services.Interfaces;
+using NetSSHTunneler.Utils.Tests.Helper;
+using System.Text.Json;
 using Xunit;
 
 namespace NetSSHTunneler.App.Tests
 {
     public class HomeControllerTests
     {
-        private readonly Mock<ILogger<HomeController>> loggerMock;
-        private readonly Mock<ISshConnector> sshConnectorMock;
-        private readonly Mock<IFileOperations> fileOperationsMock;
-        public HomeControllerTests()
+        public HomeControllerTests() { }
+
+        [Theory, AutoMoqData]
+        public void CheckConnection_ShouldBeSuccesfully(
+            SshConnectionDto sshConnectionDto,
+            HomeController sut)
         {
-            fileOperationsMock = new Mock<IFileOperations>();
-            sshConnectorMock = new Mock<ISshConnector>();
-            loggerMock = new Mock<ILogger<HomeController>>();
-        }
-
-        [Fact]
-        public void CheckConnection_ShouldBeSuccesfully()
-        {
-            // Arrange
-            var sshConnectionDto = new SshConnectionDto
-            {
-                TargetIp = "127.0.0.1"
-            };
-
-            var expected = new ConnectionStatusResponse
-            {
-                Status = true
-            };
-
-            this.sshConnectorMock.Setup(x => x.CheckConnection(sshConnectionDto)).Returns(expected);
-            var controller = this.GetHomeControllerSut();
-            
             // Act
-            var result = controller.CheckConnection(sshConnectionDto);
+            var result = sut.CheckConnection(sshConnectionDto);
 
             // Assert
             result.Result.Should().BeOfType<OkObjectResult>();
-            ((OkObjectResult)result.Result).Value.Should().BeEquivalentTo(expected);
+            ((ConnectionStatusResponse)((OkObjectResult)result.Result).Value).Status.Should().BeTrue();
         }
 
-        [Fact]
-        public void CheckConnection_NullSshConnectionDto_412Status()
+        [Theory, AutoMoqData]
+        public void CheckConnection_NullSshConnectionDto_412Status(
+            HomeController sut)
         {
-            // Arrange
-            var controller = this.GetHomeControllerSut();
-
             // Act
-            var result = controller.CheckConnection(null);
+            var result = sut.CheckConnection(null);
 
             // Assert
             result.Result.Should().BeOfType<ObjectResult>();
@@ -62,19 +42,13 @@ namespace NetSSHTunneler.App.Tests
             ((ProblemDetails)((ObjectResult)result.Result).Value).Status.Should().Be(412);
         }
 
-        [Fact]
-        public void CheckConnection_NoTargetIP_412Status()
+        [Theory, AutoMoqData]
+        public void CheckConnection_NoTargetIP_412Status(
+            [NoAutoProperties] SshConnectionDto sshConnectionDto,
+            HomeController sut)
         {
-            // Arrange
-            var sshConnectionDto = new SshConnectionDto
-            {
-                TargetIp = string.Empty
-            };
-
-            var controller = this.GetHomeControllerSut();
-
             // Act
-            var result = controller.CheckConnection(sshConnectionDto);
+            var result = sut.CheckConnection(sshConnectionDto);
 
             // Assert
             result.Result.Should().BeOfType<ObjectResult>();
@@ -82,31 +56,27 @@ namespace NetSSHTunneler.App.Tests
             ((ProblemDetails)((ObjectResult)result.Result).Value).Status.Should().Be(412);
         }
 
-        [Fact]
-        public void DeleteDashboardConfig_ShouldBeSuccesfully()
+        [Theory, AutoMoqData]
+        public void DeleteDashboardConfig_ShouldBeSuccesfully(
+            HomeController sut)
         {
-            // Arrange
-            this.fileOperationsMock.Setup(x => x.DeleteFile(It.IsAny<string>()));
-
-            var controller = this.GetHomeControllerSut();
-
             // Act
-            var result = controller.DeleteDashboardConfig();
+            var result = sut.DeleteDashboardConfig();
 
             // Assert
             result.Should().BeOfType<OkResult>();
         }
 
-        [Fact]
-        public void DeleteDashboardConfig_NoFile_ExceptionThrown()
+        [Theory, AutoMoqData]
+        public void DeleteDashboardConfig_NoFile_ExceptionThrown(
+            [Frozen] Mock<IFileOperations> fileOperationsMock,
+            HomeController sut)
         {
             // Arrange
-            this.fileOperationsMock.Setup(x => x.DeleteFile(It.IsAny<string>())).Throws(new System.Exception());
-
-            var controller = this.GetHomeControllerSut();
+            fileOperationsMock.Setup(x => x.DeleteFile(It.IsAny<string>())).Throws(new System.Exception());
 
             // Act
-            var result = controller.DeleteDashboardConfig();
+            var result = sut.DeleteDashboardConfig();
 
             // Assert
             result.Should().BeOfType<ObjectResult>();
@@ -114,46 +84,40 @@ namespace NetSSHTunneler.App.Tests
             ((ProblemDetails)((ObjectResult)result).Value).Status.Should().Be(500);
         }
 
-        [Fact]
-        public void CheckDashboard_ShouldBeSuccesfully()
+        [Theory, AutoMoqData]
+        public void CheckDashboard_ShouldBeSuccesfully(
+            DashboardStatusResponse content,
+            [Frozen] Mock<IFileOperations> fileOperationsMock,
+            HomeController sut)
         {
             // Arrange
-            var json = "{}";
-            DashboardStatusResponse expected = new(json);
-            this.fileOperationsMock.Setup(x => x.ReadFile(It.IsAny<string>(), It.IsAny<string>())).Returns(json);
-            var controller = this.GetHomeControllerSut();
+            var message = "Dashboard configured!";
+            fileOperationsMock.Setup(x => x.ReadFile(It.IsAny<string>(), It.IsAny<string>())).Returns(JsonSerializer.Serialize(content));
 
             // Act
-            var result = controller.CheckDashboard();
+            var result = sut.CheckDashboard();
 
             // Assert
             result.Result.Should().BeOfType<OkObjectResult>();
-            ((OkObjectResult)result.Result).Value.Should().BeEquivalentTo(expected);
+            ((DashboardStatusResponse)((OkObjectResult)result.Result).Value).Message.Should().BeEquivalentTo(message);
+            ((DashboardStatusResponse)((OkObjectResult)result.Result).Value).Configured.Should().BeTrue();
         }
 
-        [Fact]
-        public void CheckDashboard_NoFile_ExceptionThrown()
+        [Theory, AutoMoqData]
+        public void CheckDashboard_NoFile_ExceptionThrown(
+            [Frozen] Mock<IFileOperations> fileOperationsMock,
+            HomeController sut)
         {
             // Arrange
-            this.fileOperationsMock.Setup(x => x.ReadFile(It.IsAny<string>(), It.IsAny<string>())).Throws(new System.Exception());
-            var controller = this.GetHomeControllerSut();
+            fileOperationsMock.Setup(x => x.ReadFile(It.IsAny<string>(), It.IsAny<string>())).Throws(new System.Exception());
 
             // Act
-            var result = controller.CheckDashboard();
+            var result = sut.CheckDashboard();
 
             // Assert
             result.Result.Should().BeOfType<ObjectResult>();
             ((ObjectResult)result.Result).Value.Should().BeOfType<ProblemDetails>();
             ((ProblemDetails)((ObjectResult)result.Result).Value).Status.Should().Be(500);
-        }
-
-        private HomeController GetHomeControllerSut()
-        {
-            return new HomeController(
-                this.loggerMock.Object,
-                this.sshConnectorMock.Object,
-                this.fileOperationsMock.Object
-                );
         }
     }
 }
